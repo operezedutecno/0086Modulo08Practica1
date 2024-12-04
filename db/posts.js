e90e4f9a1a52bcfbb15bbf32ebd6a91457d46f94
Osman Pérez <operez@edutecno.com>
2024-12-04 19:42:59 -0400

    Registro y modificación de posts

--- db/posts.js
+++ db/posts.js
@@ -6,4 +6,25 @@ const list = async(userId) => {
     return posts.rows;
 }
 
-module.exports = { list }
+const show = async(postId) => {
+    const response = await conexion.query("SELECT * FROM posts WHERE id = $1", [postId]);
+    return response.rowCount > 0 ? response.rows[0] : null;
+}
+
+const register = async(title, content, userId) => {
+    const response = await conexion.query(
+            "INSERT INTO posts(title, content, user_id) VALUES($1, $2, $3) RETURNING *", 
+            [title, content, userId]
+    );
+    return response.rows;
+}
+
+const update = async(title, content, postId) => {
+    const response = await conexion.query(
+        "UPDATE posts SET title=$1, content=$2 WHERE id=$3 RETURNING *", 
+        [title,content,postId]
+    );
+    return response.rows;
+}
+
+module.exports = { list, register, show, update }
--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const secret = "secret0086";
 
 const { authentication } = require("./db/users.js")
 const jwt = require("jsonwebtoken");
-const { list } = require("./db/posts.js");
+const { list, register, show, update } = require("./db/posts.js");
 app.use(express.json());
 
 app.listen(port, () => console.log(`Aplicación en ejecución por el puerto ${port}`));
@@ -26,4 +26,48 @@ app.get("/posts", async(request, response) => {
     const payload = jwt.verify(request.headers.authorization, secret);
     const posts = await list(payload.id);
     response.json({ message: "Listado de posts", data: posts });
+})
+
+app.post("/posts", async(request, response) => {
+    if(!request.headers.authorization) {
+        return response.status(401).json({ message: "Enviar el token"});
+    }
+
+    try {
+        var payload = jwt.verify(request.headers.authorization, secret);
+    } catch (error) {
+        return response.status(401).json({ message: "Token inválido"});
+    }
+
+    const { title, content } = request.body;
+    const post = await register(title, content, payload.id);
+    response.json({ message: "Post registrado con éxito", data: post});
+});
+
+app.put("/posts/:id", async (request, response) => {
+    const token = request.headers.authorization;
+    if(!token) {
+        return response.status(401).json({ message: "Enviar el token"});
+    }
+
+    try {
+        var payload = jwt.verify(token, secret);
+    } catch (error) {
+        return response.status(401).json({ message: "Token inválido"});
+    }
+
+    const post = await show(request.params.id);
+
+    if(!post) {
+        return response.status(404).json({ message: "Post no encontrado"});
+    }
+
+    if(payload.id != post.user_id) {
+        return response.status(401).json({ message: "Usted no es el propietario de este post"});
+    }
+
+    const {title, content} = request.body
+ 
+    const data = await update(title, content, request.params.id);
+    response.json({message: "Post modificado con éxito", data: data });
 })
