819aa2ab896bb8e5078fb738d125161bed60e856
Osman Pérez <operez@edutecno.com>
2024-12-12 19:00:23 -0400

    Configuración de conexión

--- db/conexion.js
+++ db/conexion.js
@@ -1,11 +1,11 @@
 const { Pool } = require("pg")
 
 const conexion = new Pool({
-    host: 'localhost',
-    port: 5432,
-    database: 'publicaciones',
-    user: 'postgres',
-    password: 'postgres'
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD
 });
 
 module.exports = conexion;
